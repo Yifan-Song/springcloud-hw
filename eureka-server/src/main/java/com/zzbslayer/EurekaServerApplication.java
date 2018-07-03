7949c44f5a43a12eb1bdb08de3a3fceb69a13f9f
BO-PC\Bo <songyifanbj@163.com>
2018-07-03 23:24:14 +0800

    add log to eureka and zipkin

--- eureka-server/src/main/java/com/zzbslayer/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/zzbslayer/EurekaServerApplication.java
@@ -1,5 +1,8 @@
 package com.zzbslayer;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -9,6 +12,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
+		logger.info("开始加载");
 		SpringApplication.run(EurekaServerApplication.class, args);
+        logger.info("加载完毕");
 	}
 }
--- service-zipkin/src/main/java/com/zzbslayer/zipkin/ZipkinApplication.java
+++ service-zipkin/src/main/java/com/zzbslayer/zipkin/ZipkinApplication.java
@@ -1,5 +1,7 @@
 package com.zzbslayer.zipkin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin.server.EnableZipkinServer;
@@ -10,6 +12,8 @@ import zipkin.server.EnableZipkinServer;
 public class ZipkinApplication {
 
 	public static void main(String[] args) {
+		logger.info("开始加载");
 		SpringApplication.run(ZipkinApplication.class, args);
+        logger.info("加载完毕");
 	}
 }
